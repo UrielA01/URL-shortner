b5e274d64a8e9170a05f7167fdd74e810272f6f4
UrielA01 <uriel.azar01@gmail.com>
2023-09-17 13:13:27 +0300

    mongo db

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -1,27 +1,27 @@
-const generateShortUrl = (length:number) => {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
+const generateShortUrl = (length: number) => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
 }
 
-const doesValueExistInArray = (array:Array<any>, property, value): boolean => {
-    const targetValue = value;
-    const propertyName = property;
-    
-    return array.some((obj) => obj[propertyName] === targetValue);    
+const doesValueExistInArray = (array: Array<any>, property, value): boolean => {
+  const targetValue = value;
+  const propertyName = property;
+
+  return array.some((obj) => obj[propertyName] === targetValue);
 }
 
 const findObjectInArray = <T>(
-    arr: T[],
-    key: keyof T,
-    value: T[keyof T]
-  ): T | null => arr.find(obj => obj[key] === value) || null;
+  arr: T[],
+  key: keyof T,
+  value: T[keyof T]
+): T | null => arr.find(obj => obj[key] === value) || null;
 
 
-export {generateShortUrl, doesValueExistInArray, findObjectInArray};
+export { generateShortUrl, doesValueExistInArray, findObjectInArray };
--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,73 @@
 import express from 'express';
-import {generateShortUrl, doesValueExistInArray, findObjectInArray} from './helperFunctions'
+import { generateShortUrl, doesValueExistInArray, findObjectInArray } from './helperFunctions'
 import { body, validationResult } from 'express-validator';
+import "./DB/index";
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-interface ShortUrl {
-    originalURL: string
-    generatedID: string
-    shortURL: string
-  }
-
-const URLs:Array<ShortUrl> = []
-
-const validateURL = () => {
-    return [
-      body('url')
-        .isURL()
-        .withMessage('Invalid URL')
-        .exists()
-        .withMessage('URL is required')
-        .trim()
-        .custom(async url => {
-            const existingUrl = await doesValueExistInArray(URLs, "originalURL", url);
-            if (existingUrl) {
-                const shortUrl = findObjectInArray(URLs, "originalURL", url)
-                throw new Error(`URL already in the DB: ${shortUrl.shortURL}`);
-            }
-        }),
-        (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-        }
-        next();
-    }
-    ];
-  }
-  
+const LOCALURL = "http://localhost:3000/";
 
 app.use(express.json());
+
+interface ShortUrl {
+  originalURL: string
+  generatedID: string
+  shortURL: string
+}
+
+const URLs: Array<ShortUrl> = []
+
+const validateURL = () => {
+  return [
+    body('url')
+      .isURL()
+      .withMessage('Invalid URL')
+      .exists()
+      .withMessage('URL is required')
+      .trim()
+      .custom(async url => {
+        const existingUrl = await doesValueExistInArray(URLs, "originalURL", url);
+        if (existingUrl) {
+          const shortUrl = findObjectInArray(URLs, "originalURL", url)
+          throw new Error(`URL already in the DB: ${shortUrl.shortURL}`);
+        }
+      }),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    }
+  ];
+}
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express with TypeScript!');
+  res.send('Hello, Express with TypeScript!');
 });
 
 app.post('/url', validateURL(),
-        (req, res) => {
-        const { url } = req.body;
-        const ID = generateShortUrl(5);
-        const shortURL: string = "http://localhost:3000/" + ID;
-        const newUrl: ShortUrl = {
-            originalURL: url,
-            generatedID: ID,
-            shortURL: shortURL
-        };
+  (req, res) => {
+    const { url } = req.body;
+    const ID = generateShortUrl(5);
+    const shortURL: string = LOCALURL + ID;
+    const newUrl: ShortUrl = {
+      originalURL: url,
+      generatedID: ID,
+      shortURL: shortURL
+    };
 
-        URLs.push(newUrl);
-
-        console.log(URLs);
-        
-        
-        res.sendStatus(201);
-});
+    URLs.push(newUrl);
+    res.send(newUrl.generatedID);
+  });
 
 app.get('/:urlID', (req, res) => {
-    const { urlID } = req.params;
-    const shortURL:ShortUrl = findObjectInArray(URLs, "generatedID", urlID);
-    res.send(shortURL.originalURL);
+  const { urlID } = req.params;
+  const shortURL: ShortUrl = findObjectInArray(URLs, "generatedID", urlID);
+  res.send(shortURL.originalURL);
 });
