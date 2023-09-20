641b341c7a752143838938a0442ae5ee71b6b2a9
UrielA01 <uriel.azar01@gmail.com>
2023-09-20 23:34:52 +0300

    change from console.log to console.error

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -1,3 +1,3 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/short_urls').then(() => console.log("Connect to mongoDB")).catch((err) => console.log(err))
+mongoose.connect('mongodb://localhost:27017/short_urls').then(() => console.log("Connect to mongoDB")).catch((err) => console.error(err))
